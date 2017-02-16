a6de9a1912355e20eeac098e759e263e0aa290c6
Vladislav <azzazzelko@gmail.com>
2017-02-16 17:59:11 +0300

    mainpage header, some tabs, some selects

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 $(function() {
 
+	/* Select */
+
+	$('.select').select2();
+
+	/* Dropdown */
+	$('.tab-content .dropdown .dropdown-toggle').on('click', function (e) {
+		e.preventDefault();
+		$(this).parent().toggleClass('open');
+	});
+
+	$('body').on('click', function (e) {
+		if ( !$('.tab-content .dropdown').is(e.target) && $('.tab-content .dropdown').has(e.target).length === 0 && $('.open').has(e.target).length === 0 ) {
+			$('.tab-content .dropdown').removeClass('open');
+		};
+	});
 
 }());
