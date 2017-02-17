c83c3c20c920c27ca1cfaabebe067dfc461c4f3e
Vladislav <azzazzelko@gmail.com>
2017-02-17 17:52:18 +0300

    main page complete

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,9 @@ $(function() {
 	/* Select */
 
 	$('.select').select2();
+	$('.hits-select').select2({
+		dropdownCssClass: "hits-select-select2-dropdown"
+	})
 
 	/* Dropdown */
 	$('.tab-content .dropdown .dropdown-toggle').on('click', function (e) {
@@ -10,10 +13,28 @@ $(function() {
 		$(this).parent().toggleClass('open');
 	});
 
+	$('.tab-dropdown-close-btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).parent().parent().parent().toggleClass('open')
+	});
+
 	$('body').on('click', function (e) {
 		if ( !$('.tab-content .dropdown').is(e.target) && $('.tab-content .dropdown').has(e.target).length === 0 && $('.open').has(e.target).length === 0 ) {
 			$('.tab-content .dropdown').removeClass('open');
 		};
+	});
+
+	/* check all */
+	$('#check_allproducer').on('change', function(e){
+		$(this).parent().parent().siblings('.row').find('input').prop('checked', this.checked);
+	})
+
+	/* filter-reset */
+	$('.tab-dropdown-clear-btn').on("click", function(e){
+		e.preventDefault();
+		var $this = $(this);
+
+		$this.parent().parent().find('input').attr('checked', false);
 	});
 
 }());
