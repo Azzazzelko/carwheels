32d34b89c804413841e077393a5126cc470eb472
Vladislav <azzazzelko@gmail.com>
2017-03-14 11:42:54 +0300

    added functionality for dynamic select

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 $(function() {
 
 	/* Select */
-
 	$('.select').select2();
 	$('.hits-select').select2({
 		dropdownCssClass: "hits-select-select2-dropdown"
@@ -49,14 +48,206 @@ $(function() {
 		singleItem : true
 	});
 
-	/* circle text */
-	if ( $('.wheel-circle-text').length ){
-		$('.wheel-circle-text').circleType({radius: 200});
-	}
-	
 	/* all sizes btn */
 	$('.card-block__info-sizes-btn').on('click', function(e){
 		$('a[href=#sizes]').click();
 	});
+
+	/* circle text */
+	if ( $('.wheel-circle-text').length ){
+		/* init */
+		var sel1 = $('#selWidth').val();
+		var sel2 = $('#selHeight').val();
+		var sel3 = $('#selDiameter').val();
+
+		if(sel1 < 100)
+		{
+			$("#wheel-circle-text").html(sel1 + '&nbsp;x&nbsp;' + sel2 + '&nbsp;R' + sel3);
+		}
+		else
+		{
+			$("#wheel-circle-text").html(sel1 + '&nbsp;/&nbsp;' + sel2 + '&nbsp;R' + sel3);
+		}
+
+		$('#wheel-circle-text').circleType({radius: 200});
+
+		/* on change */
+		$('#selWidth').on('change', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var sel3 = $('#selDiameter').val();
+			if(sel1 < 100)
+			{
+				$("#wheel-circle-text").html(sel1 + '&nbsp;x&nbsp;' + sel2 + '&nbsp;R' + sel3);
+			}
+			else
+			{
+				$("#wheel-circle-text").html(sel1 + '&nbsp;/&nbsp;' + sel2 + '&nbsp;R' + sel3);
+			}
+			$('#wheel-circle-text').circleType({radius: 200});
+		});
+		$('#selHeight').on('change', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var sel3 = $('#selDiameter').val();
+			if(sel1 < 100)
+			{
+				$("#wheel-circle-text").html(sel1 + '&nbsp;x&nbsp;' + sel2 + '&nbsp;R' + sel3);
+			}
+			else
+			{
+				$("#wheel-circle-text").html(sel1 + '&nbsp;/&nbsp;' + sel2 + '&nbsp;R' + sel3);
+			}
+			$('#wheel-circle-text').circleType({radius: 200});
+		});
+		$('#selDiameter').on('change', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var sel3 = $('#selDiameter').val();
+			if(sel1 < 100)
+			{
+				$("#wheel-circle-text").html(sel1 + '&nbsp;x&nbsp;' + sel2 + '&nbsp;R' + sel3);
+			}
+			else
+			{
+				$("#wheel-circle-text").html(sel1 + '&nbsp;/&nbsp;' + sel2 + '&nbsp;R' + sel3);
+			}
+			$('#wheel-circle-text').circleType({radius: 200});
+		});
+
+		/* hovers */
+		$('#select2-selWidth-container').parent().on('mouseover', function(){
+			var sel1 = $('#selWidth').val();
+			var n1 = sel1.length;
+			for(var i=1;i<=n1;i++){
+				$('.char'+i).css('color', '#225aa4');
+			}
+		});
+		$('#select2-selWidth-container').parent().on('mouseout', function(){
+			var sel1 = $('#selWidth').val();
+			var n1 = sel1.length;
+			for(var i=1;i<=n1;i++){
+				$('.char'+i).css('color', '#fff');
+			}
+		});	
+		$('#select2-selWidth-container').parent().on('click', function(){
+			$('#select2-selWidth-results').on('mouseover', function(){
+				var sel1 = $('#selWidth').val();
+				var n1 = sel1.length;
+				for(var i=1;i<=n1;i++){
+					$('.char'+i).css('color', '#225aa4');
+				}
+			});
+			$('#select2-selWidth-results').on('mouseout', function(){
+				var sel1 = $('#selWidth').val();
+				var n1 = sel1.length;
+				for(var i=1;i<=n1;i++){
+					$('.char'+i).css('color', '#fff');
+				}
+			});
+		});
+
+		$('#select2-selHeight-container').parent().on('mouseover', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var n1 = sel1.length;
+			var n2 = sel2.length;
+			var start = 1 + n1 + 3;
+			var end = start + n2;
+			for(var i=start;i<end;i++){
+				$('.char'+i).css('color', '#225aa4');
+			}
+		});
+		$('#select2-selHeight-container').parent().on('mouseout', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var n1 = sel1.length;
+			var n2 = sel2.length;
+			var start = 1 + n1 + 3;
+			var end = start + n2;
+			for(var i=start;i<end;i++){
+				$('.char'+i).css('color', '#fff');
+			}
+		});
+		$('#select2-selHeight-container').parent().on('click', function(){
+			$('#select2-selHeight-results').on('mouseover', function(){
+				var sel1 = $('#selWidth').val();
+				var sel2 = $('#selHeight').val();
+				var n1 = sel1.length;
+				var n2 = sel2.length;
+				var start = 1 + n1 + 3;
+				var end = start + n2;
+				for(var i=start;i<end;i++){
+					$('.char'+i).css('color', '#225aa4');
+				}
+			});
+			$('#select2-selHeight-results').on('mouseout', function(){
+				var sel1 = $('#selWidth').val();
+				var sel2 = $('#selHeight').val();
+				var n1 = sel1.length;
+				var n2 = sel2.length;
+				var start = 1 + n1 + 3;
+				var end = start + n2;
+				for(var i=start;i<end;i++){
+					$('.char'+i).css('color', '#fff');
+				}
+			});
+		});
+
+		$('#select2-selDiameter-container').parent().on('mouseover', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var sel3 = $('#selDiameter').val();
+			var n1 = sel1.length;
+			var n2 = sel2.length;
+			var n3 = sel3.length;
+			var start = 1 + n1 + 3 + n2 + 1;
+			var end = start + 1 + n3;
+			for(var i=start;i<end;i++){
+				$('.char'+i).css('color', '#225aa4');
+			}
+		});
+		$('#select2-selDiameter-container').parent().on('mouseout', function(){
+			var sel1 = $('#selWidth').val();
+			var sel2 = $('#selHeight').val();
+			var sel3 = $('#selDiameter').val();
+			var n1 = sel1.length;
+			var n2 = sel2.length;
+			var n3 = sel3.length;
+			var start = 1 + n1 + 3 + n2 + 1;
+			var end = start + 1 + n3;
+			for(var i=start;i<end;i++){
+				$('.char'+i).css('color', '#fff');
+			}
+		});
+		$('#select2-selDiameter-container').parent().on('click', function(){
+			$('#select2-selDiameter-results').on('mouseover', function(){
+				var sel1 = $('#selWidth').val();
+				var sel2 = $('#selHeight').val();
+				var sel3 = $('#selDiameter').val();
+				var n1 = sel1.length;
+				var n2 = sel2.length;
+				var n3 = sel3.length;
+				var start = 1 + n1 + 3 + n2 + 1;
+				var end = start + 1 + n3;
+				for(var i=start;i<end;i++){
+					$('.char'+i).css('color', '#225aa4');
+				}
+			});
+			$('#select2-selDiameter-results').on('mouseout', function(){
+				var sel1 = $('#selWidth').val();
+				var sel2 = $('#selHeight').val();
+				var sel3 = $('#selDiameter').val();
+				var n1 = sel1.length;
+				var n2 = sel2.length;
+				var n3 = sel3.length;
+				var start = 1 + n1 + 3 + n2 + 1;
+				var end = start + 1 + n3;
+				for(var i=start;i<end;i++){
+					$('.char'+i).css('color', '#fff');
+				}
+			});
+		});	
+	};
 
 }());
